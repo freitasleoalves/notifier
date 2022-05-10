17eab0fbc61245de91b1cda57e10eee0049ebeac
freitasleoalves <73323522+freitasleoalves@users.noreply.github.com>
2022-05-10 13:18:53 -0300

    Alterado ordem de condig no user data

--- userdata-notifier.sh
+++ userdata-notifier.sh
@@ -9,14 +9,6 @@ yum install -y httpd mysql php php-mysql php-mysqlnd php-pdo telnet tree git
 # Enable Apache service to start after reboot
 sudo systemctl enable httpd
 
-# Config composer SNS
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup. php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-
-sudo mv composer.phar /usr/local/bin/composer
-
 # Config connect to DB
 cat <<EOT >> /var/www/config.php
 <?php
@@ -28,6 +20,15 @@ define('DB_DATABASE', 'notifier');
 
 ?>
 EOT
+
+# Config composer SNS
+cd /home
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+# php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup. php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+sudo mv composer.phar /usr/local/bin/composer
 
 # Install application
 cd /tmp
